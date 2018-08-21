6fc281daaae2e8d8e33318d361bf42419f78e381
connorgilheany <connorgilheany@gmail.com>
2018-08-21 15:39:33 -0400

    Reordered comment

--- routes/Service.js
+++ routes/Service.js
@@ -8,14 +8,14 @@ class Service {
     /*
      * method: The http method that this Service handles. GET, POST, PUT, etc
      * route: The URI path that this Service handles. The route is appended to the baseRoute of the RouteManager that creates this service
-     * middleware: An array of middleware to apply before handling this Service call
      * handler: The function that handles this Service call.
+     * middleware: An array of middleware to apply before handling this Service call
      */
     constructor(method, route, handler, middleware=[]) {
         this.method = method.toLowerCase();
         this.route = route;
-        this.middleware = middleware;
         this.handler = handler;
+        this.middleware = middleware;
     }
 }
 module.exports = Service;
