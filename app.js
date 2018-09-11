16fdceda08464e0e5dc072f3fd00acd483c4e191
connorgilheany <connorgilheany@gmail.com>
2018-09-11 11:54:08 -0400

    Added CORS policies to allow credentials

--- app.js
+++ app.js
@@ -15,9 +15,9 @@ app.use(cookieParser());
 
 app.use('/', (req, res, next) => {
     //console.log(req);
-    console.log('Received pre-flight OPTIONS request.');
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3001');
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
 });
 
